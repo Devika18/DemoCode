1570e37ebc9bce14a2ec7a479de7b2d3e6dfc2b8
devikakarpe <devikankarpe@gmail.com>
2023-03-05 00:06:22 -0800

    demo code

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a/b;
+        int mod = a % b;
+
+        System.out.println("The addition is "+sum);
+        System.out.println("The substraction is "+sub);
+        System.out.println("The multiplication is "+mul);
+        System.out.println("The quotient is "+div);
+        System.out.println("The reminder is "+mod);
+    }
+}
